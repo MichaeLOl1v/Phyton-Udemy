9c06d72042b3768bafd01e6e56ef7e692d602309
MichaeLOl1v <125513389+MichaeLOl1v@users.noreply.github.com>
2025-01-29 14:20:36 -0300

    Add files via upload

---
+++ Phyton Udemy/importando-modulo.py
@@ -0,0 +1,16 @@
+import math  # Math = funções matematicas
+
+print(math.ceil(2.7))
+
+
+
+
+
+
+# print(math.factorial(4)) 
+
+# fatorial = 4 * 3 * 2 * 1 # Digamos que a forma manual do que está feito na linha acima
+# print(fatorial)
+
+# Qual é o numero fatorial de 4
+# 4 * 3 * 2 * 1 = 24
